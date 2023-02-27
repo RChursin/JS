f2e87db6a832f10b65b68f764bed2fc5392959a4
ganstacoder <ganstacoder@gmail.com>
2023-02-27 11:43:37 -0800

    Init

---
+++ vars.js
@@ -0,0 +1,41 @@
+// JavaScript data types
+
+// String example
+var myName = "John Doe";
+console.log(myName);
+
+// Number example
+var myAge = 25;
+console.log(myAge);
+
+// Boolean example
+var isMale = true;
+console.log(isMale);
+
+// Array example
+var myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+
+// Object example
+var myObject = {name: "John Doe", age: 25};
+console.log(myObject);
+
+// Null example
+var myNull = null;
+console.log(myNull);
+
+// Undefined example
+var myUndefined;
+console.log(myUndefined);
+
+// NaN example
+var myNaN = 0 / 0;
+console.log(myNaN);
+
+// Infinity example
+var myInfinity = 1 / 0;
+console.log(myInfinity);
+
+// -Infinity example
+var myNegativeInfinity = -1 / 0;
+console.log(myNegativeInfinity);
